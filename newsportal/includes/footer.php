d77128c82b2174f0be8498de49abd78efd3668a7
Pgirardin <ndayisengapgirard@gmail.com>
2021-08-24 19:44:59 +0300

    Initial commit

---
+++ newsportal/includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-blue"style="background-color: #4D774E;">
+      <div class="container"style="background-color: #4D774E;">
+        <p class="m-0 text-center text-white"style="background-color: #4D774E;">Copyright &copy; New Technology 2021</p>
+      </div>
+      <!-- /.container -->
+    </footer>
---
+++ newsportal/includes/header.php
@@ -0,0 +1,27 @@
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-Medium Green fixed-top" style="background-color: #4D774E;">
+      <div class="container">
+        <a style="color: #b1bdb2" class="nav-link" href="index.php">Home</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a style="color: #b1bdb2" class="nav-link" href="about-us.php">BoobyToken</a>
+            </li>
+			 <li class="nav-item">
+              <a style="color: #b1bdb2" class="nav-link" href="https://drive.google.com/file/d/1TR_wcd3PwBIBPT6kkq1CChy2hi6WbkXB/view?usp=sharing">Whitepaper</a>
+            </li>
+			
+                 <li class="nav-item">
+              <a style="color: #b1bdb2" class="nav-link" href="index.php">News</a>
+            </li>
+             <li class="nav-item">
+              <a style="color: #b1bdb2" class="nav-link" href="contact-us.php">Contact us</a>
+            </li>
+  
+  
+          </ul>
+        </div>
+      </div>
+    </nav>
